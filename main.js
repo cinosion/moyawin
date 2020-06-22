5099120c923d62b4826aa8c34b7f339b518a66d1
cinosion <sugimoto.sion.21010@gmail.com>
2020-06-23 01:19:06 +0900

    fix:download

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var container = document.getElementById('canvass');
+// var container4 = document.getElementById('canvass4');
 var canvas = document.getElementById('moyawin2');
+// var canvas4 = document.getElementById('moyawin4');
 // var obj = document.createElement('canvas');
 if (canvas.getContext){
   var ctx = canvas.getContext('2d');
+  // var ctx4 = canvas4.getContext('2d');
 }
 var radioStyle = 'red'
 
@@ -23,11 +26,22 @@ var render = function(word='憲法改正', radio=radioStyle, save='whole'){
   //親要素のサイズをCanvasに指定
   canvas.width = container.clientWidth;
   canvas.height = container.clientHeight;
-
   var scale = canvas.width / img.width;
   ctx.setTransform(scale, 0, 0, scale, 0, 0);
   ctx.drawImage(img, 0,0);
+/*
+  ctx4.clearRect(0, 0, canvas4.width, canvas4.height);
+  canvas4.width = container4.clientWidth;
+  canvas4.height = container4.clientHeigt;
+  var scale4 = canvas4.width / img.width;
+  ctx4.setTransform(scale, 0, 0, scale, 0, 0);
+  ctx4.drawImage(img, 88,1500, 800,450, 0,0, 1120,630);
+*/
+  canvasTextDraw(word, radio)
+  // changeImage();
+}
 
+var canvasTextDraw = function(word, radio){
   ctx.font = "38px sanserif";
   // ctx.fillText(word, 200, 1800);
   var wordList = word.split('\n');
@@ -52,9 +66,7 @@ var render = function(word='憲法改正', radio=radioStyle, save='whole'){
       });
     });
   }
-  changeImage();
 }
-
 
 var moyawin_drawString = function(){
   var word = document.form1.textarea1.value;
@@ -68,20 +80,20 @@ var radioUpdater = function(){
   moyawin_drawString();
 }
 
-
+/*
 var changeImage = function(){
   var png = canvas.toDataURL();
   document.getElementById("newImg").src = png;
-/*
+
   var downloadLink = document.getElementById('download_link');
   var filename = 'moyawin.png';
 
   downloadLink.href = canvas.toDataURL('image/png');
   downloadLink.download = filename;
   downloadLink.click();
-*/
-}
 
+}
+*/
 var moyawin_save_full = function(){
   var downloadLink = document.getElementById('download_link');
   var filename = 'moyawin.png';
@@ -90,12 +102,20 @@ var moyawin_save_full = function(){
   downloadLink.download = filename;
   downloadLink.click();
 }
-
+/*
 var moyawin_save_four = function(){
   var downloadLink = document.getElementById('download_link');
-  var filename = 'moyawin.png';
-}
+  var filename = 'moyawin4.png';
 
+  ctx.drawImage(img, 64,1500, 800,450, 0,0, 960,540);
+  var word = document.form1.textarea1.value;
+  canvasTextDraw(word,radioStyle)
+
+  downloadLink.href = canvas.toDataURL('image/png');
+  downloadLink.download = filename;
+  downloadLink.click();
+}
+*/
 var main = function(){
   img.addEventListener('load', render, false);
   window.addEventListener('resize', render, false);
